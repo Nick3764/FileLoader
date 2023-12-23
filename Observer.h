32aa0eb0784be2341b981d7add029e9b7fdd17af
Nick3777 <brandonicco@gmail.com>
2023-12-23 20:25:53 +0100

    Configured Observer and Subject.

--- Observer.h
+++ Observer.h
@@ -1,8 +1,11 @@
-//
-// Created by Utente on 23/12/2023.
-//
 
 #ifndef LOADER_OBSERVER_H
 #define LOADER_OBSERVER_H
+
+class Observer {
+public:
+    virtual ~Observer() = default;
+    virtual void update() = 0;
+};
 
 #endif //LOADER_OBSERVER_H
--- Subject.h
+++ Subject.h
@@ -1,8 +1,16 @@
-//
-// Created by Utente on 23/12/2023.
-//
 
 #ifndef LOADER_SUBJECT_H
 #define LOADER_SUBJECT_H
+
+#include "Observer.h"
+
+class Subject {
+protected:
+    virtual ~Subject() = default;
+public:
+    virtual void addObserver(Observer * o) = 0;
+    virtual void removeObserver(Observer * o) = 0;
+    virtual void notifyObservers() const = 0;
+};
 
 #endif //LOADER_SUBJECT_H
