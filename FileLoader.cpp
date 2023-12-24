81a16b6946038b2c7ba92e7099ccb7200bb76385
Nick3777 <brandonicco@gmail.com>
2023-12-24 03:33:56 +0100

    Added a lot of GUI logic with new functions and buttons action.

--- FileLoader.cpp
+++ FileLoader.cpp
@@ -1,3 +1,57 @@
 //
 // Created by Utente on 23/12/2023.
 //
+#include "FileLoader.h"
+#include <QFile>
+#include <QTextStream>
+#include <QFileInfo>
+
+FileLoader::FileLoader(QObject* parent) : QObject(parent), filesNumber(0) {}
+
+void FileLoader::addObserver(Observer *o) {
+    observers.push_back(o);
+}
+
+void FileLoader::removeObserver(Observer *o) {
+    observers.removeAll(o);
+}
+
+void FileLoader::notifyObservers(int value) {
+    for(Observer* observer : observers)
+        observer->update(value);
+}
+
+void FileLoader::load(const QVector<QString>& files) {
+    int totalSize = 0;
+    for (const QString& filePath : files) {
+        QFileInfo fileInfo(filePath);
+        if (!fileInfo.exists() || fileInfo.suffix().toLower() != "txt") {
+            continue;
+        }
+        totalSize += fileInfo.size(); // Aggiungi la dimensione del file alla dimensione totale
+    }
+
+    filesNumber = files.size();
+    int loadedSize = 0;
+
+    for (const QString& filePath : files) {
+        QFileInfo fileInfo(filePath);
+        if (!fileInfo.exists() || fileInfo.suffix().toLower() != "txt") {
+            continue;
+        }
+
+        QFile file(filePath);
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            loadedSize += fileInfo.size(); // Aggiorna la dimensione caricata
+
+            int progress = (loadedSize * 100) / totalSize;
+            notifyObservers(progress);
+
+            file.close();
+        }
+    }
+}
+
+
+
+
--- FileLoader.h
+++ FileLoader.h
@@ -1,8 +1,30 @@
-//
-// Created by Utente on 23/12/2023.
-//
 
 #ifndef LOADER_FILELOADER_H
 #define LOADER_FILELOADER_H
+
+#include "Subject.h"
+#include "ResourceFile.h"
+#include <QObject>
+#include <QVector>
+#include <list>
+#include <QString>
+
+class FileLoader :  public QObject, public Subject{
+    Q_OBJECT
+
+public:
+    explicit FileLoader(QObject* parent = nullptr);
+
+    void addObserver(Observer* o) override;
+    void removeObserver(Observer* o) override;
+    void notifyObservers(int value) override;
+
+public slots:
+    void load(const QVector<QString>& files);
+
+private:
+    int filesNumber;
+    QVector<Observer*> observers;
+};
 
 #endif //LOADER_FILELOADER_H
