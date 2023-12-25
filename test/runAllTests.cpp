4ade62d00ee7a39aad7ada289d6b2e01a0ba8462
Nick3777 <brandonicco@gmail.com>
2023-12-25 02:43:57 +0100

    Adjusted Cmake files and prepared the runAllTests.

--- test/runAllTests.cpp
+++ test/runAllTests.cpp
@@ -1,3 +1,7 @@
-//
-// Created by Utente on 25/12/2023.
-//
+
+#include "gtest/gtest.h"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
