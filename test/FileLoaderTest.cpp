99452aa37b9f716519205247880196b18a9e350a
Nick3777 <brandonicco@gmail.com>
2023-12-25 03:15:32 +0100

    Adjusted Cmake files and prepared the runAllTests.

--- test/FileLoaderTest.cpp
+++ test/FileLoaderTest.cpp
@@ -1,3 +1,17 @@
-//
-// Created by Utente on 25/12/2023.
-//
+#include <gtest/gtest.h>
+#include "../FileLoader.h"
+#include <../QVector>
+
+// Test case per verificare il caricamento dei file
+TEST(FileLoaderTest, LoadFiles) {
+    FileLoader fileLoader;
+    QVector <QString> files;
+// Aggiungi file validi alla lista
+    files.push_back("path/to/valid_file1.txt");
+    files.push_back("path/to/valid_file2.txt");
+
+// Verifica che il caricamento avvenga correttamente
+    EXPECT_NO_THROW({
+                        fileLoader.load(files);
+                    });
+}
