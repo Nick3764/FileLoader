7620ad3eb26703890515cd3de31cad6762afefa1
Nick3777 <brandonicco@gmail.com>
2024-01-05 15:21:04 +0100

    Added a test observer class to see that the update method is called a correct amount of time.

--- test/FileLoaderTest.cpp
+++ test/FileLoaderTest.cpp
@@ -34,3 +34,45 @@ TEST(FileLoaderTest, LoadInvalidFiles) {
     // Verifica che il progresso sia rimasto a 0
     EXPECT_EQ(fileLoader.getProgress(), 0);
 }
+
+class TestObserver : public Observer {
+public:
+    TestObserver() : updateCalls(0) {}
+
+    void update() override {
+        // Incrementa il contatore ogni volta che viene chiamato update()
+        updateCalls++;
+    }
+
+    int getUpdateCalls() const {
+        return updateCalls;
+    }
+
+private:
+    int updateCalls;
+};
+
+TEST(FileLoaderTest, CheckUpdateCalls){
+    FileLoader fileLoader;
+    TestObserver testObserver;
+    fileLoader.addObserver(&testObserver);
+    QVector <QString> files;
+
+    // Aggiunta file validi alla lista
+    files.push_back("C:/Users/Utente/Desktop/1.txt");
+    files.push_back("C:/Users/Utente/Desktop/2.txt");
+
+    int expectedUpdateCalls = 0;
+    for (const QString& filePath : files) {
+        QFile file(filePath);
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            while (!file.atEnd()) {
+                file.readLine();
+                expectedUpdateCalls++;
+            }
+            file.close();
+        }
+    }
+    fileLoader.load(files);
+    ASSERT_EQ(testObserver.getUpdateCalls(), expectedUpdateCalls);
+}
